648510da0f871e69223db9f61c2c2b74bd8de9c8
Mauricio <smartprecast.nb3@gmail.com>
2020-09-16 16:47:21 -0300

    feature: mobile (expo with typescript)

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import { AppLoading } from 'expo';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold  } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
+
+const App: React.FC = () => {
+  let [fonstLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+
+  if (!fonstLoaded) {
+    return <AppLoading />
+  } 
+  
+  return (
+    <>
+      <AppStack />
+      <StatusBar style="light" />
+    </>
+  );
+  
+}
+
+export default App;
